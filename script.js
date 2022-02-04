48be850e565f9fe84c478dd75bc43ab86b4d7714
Imtiaz <imtiaz.rashid.89@gmail.com>
2022-02-04 12:23:07 -0500

    fixed all of the link issues, now every section scrolls to the page smoothly.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+const navigationMenu = document.querySelector('.menu-item-container').children
+const exploreButton = document.getElementById('explore-projects')
+
+const clickHandler = (event) => {
+    event.preventDefault()
+    const element = event.target
+    const href = element.getAttribute("href")  
+    const elementTop = document.querySelector(href).offsetTop
+    
+    scroll({
+        top: elementTop,
+        behavior: "smooth",
+    })
+}
+
+for(const link of navigationMenu) {
+    const anchor = link.children[0]
+    anchor.addEventListener("click", clickHandler)
+}
+
+exploreButton.addEventListener("click", clickHandler)
+
+
+
+
+
+// () => {
+//     projects.scrollIntoView({
+//         behavior: "smooth",
+//         block: "start"
+//     });
+// }
