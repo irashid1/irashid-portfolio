72c6601673ff2968200bb57eba16aae74853a5b0
Imtiaz <imtiaz.rashid.89@gmail.com>
2022-02-10 14:45:11 -0500

    added the hamburger menu functionality.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const navigationMenu = document.querySelector('.menu-item-container').children
+const navigationMenuItem = document.querySelector('.menu-item-container').children
+const navigationMenu = document.querySelector('.menu-item-container')
 const exploreButton = document.getElementById('explore-projects')
+const hamburgerMenu = document.querySelector('.menu-icon')
+const listElement = document.querySelectorAll('.menu-item')
+
+hamburgerMenu.addEventListener("click", () => {
+    navigationMenu.classList.toggle("isActive");
+})
+
+listElement.forEach((li) => {
+    console.log(li)
+    li.addEventListener("click", () => {
+        navigationMenu.classList.remove("isActive")
+    })
+
+})
 
 const clickHandler = (event) => {
     event.preventDefault()
@@ -13,20 +28,9 @@ const clickHandler = (event) => {
     })
 }
 
-for(const link of navigationMenu) {
+for(const link of navigationMenuItem) {
     const anchor = link.children[0]
     anchor.addEventListener("click", clickHandler)
 }
 
 exploreButton.addEventListener("click", clickHandler)
-
-
-
-
-
-// () => {
-//     projects.scrollIntoView({
-//         behavior: "smooth",
-//         block: "start"
-//     });
-// }
